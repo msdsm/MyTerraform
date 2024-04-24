7ca15e15020f642d068dc7bd17dd8d9faad71cbd
msdsm <y.msd0110@gmail.com>
2024-04-24 15:04:34 +0900

    ecs作成

---
+++ ecr-ecs/modules/ecs/variables.tf
@@ -0,0 +1,34 @@
+# Global
+variable "region" {}
+variable "name_prefix" {}
+variable "webapp_port" {}
+
+# Tags
+variable "tag_name" {}
+variable "tag_group" {}
+variable "webapp_port" {}
+
+# Task
+variable "ecr_repository_uri" {}
+variable "execution_role_arn" {}
+
+# Service
+variable "logs_group_name" {}
+variable "tg_arn" {}
+variable "public_1_id" {}
+variable "public_2_id" {}
+variable "sg_id" {}
+
+locals {
+  ecs_cluster_name              = "${var.name_prefix}-cluster"
+  task_definitions_filepath     = "${path.module}/task_definition.json"
+  task_definitions_name         = "${var.name_prefix}-task"
+  task_cpu                      = 256
+  task_memory                   = 512
+  task_log_driver               = "awslogs"
+  task_network_mode             = "awsvpc"
+  task_requires_compatibilities = "FARGATE"
+
+  service_name  = "${var.name_prefix}-service"
+  service_count = 1
+}
